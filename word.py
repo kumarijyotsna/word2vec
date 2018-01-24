d9ac85cfa952e0c7e8296ec76ec128a25babec67
kumarijyotsna <simmikumari1997@gmail.com>
2018-01-25 00:11:30 +0530

    first commit

---
+++ word.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import, division, print_function
+import codecs
+import glob
+import logging
+import multiprocessing
+import os
+import pprint
+import re
+import nltk
+import gensim.models.word2vec as w2v
+import sklearn.manifold
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+nltk.download("punkt")
+nltk.download("stopwords")
+
+book_filenames = sorted(glob.glob("data/*.txt"))
+print("Found books:"),book_filenames
+corpus_raw = u""
+for book_filename in book_filenames:
+    print("Reading '{0}'...".format(book_filename))
+    with codecs.open(book_filename, "r", "utf-8") as book_file:
+        corpus_raw += book_file.read()
+    print("Corpus is now {0} characters long".format(len(corpus_raw)))
+    print()
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+raw_sentences = tokenizer.tokenize(corpus_raw)
+#convert into a list of words
+#rtemove unnnecessary,, split into words, no hyphens
+#list of words
+def sentence_to_wordlist(raw):
+    clean = re.sub("[^a-zA-Z]"," ", raw)
+    words = clean.split()
+    return words
+#sentence where each word is tokenized
+sentences = []
+for raw_sentence in raw_sentences:
+    if len(raw_sentence) > 0:
+        sentences.append(sentence_to_wordlist(raw_sentence))
+print(raw_sentences[5])
+print(sentence_to_wordlist(raw_sentences[5]))
+token_count = sum([len(sentence) for sentence in sentences])
+print("The book corpus contains {0:,} tokens".format(token_count))
+#ONCE we have vectors
+#step 3 - build model
+#3 main tasks that vectors help with
+#DISTANCE, SIMILARITY, RANKING
+
+# Dimensionality of the resulting word vectors.
+#more dimensions, more computationally expensive to train
+#but also more accurate
+#more dimensions = more generalized
+num_features = 300
+# Minimum word count threshold.
+min_word_count = 3
+
+# Number of threads to run in parallel.
+#more workers, faster we train
+num_workers = multiprocessing.cpu_count()
+print ("num"), num_workers
+# Context window length.
+context_size = 7
+
+# Downsample setting for frequent words.
+#0 - 1e-5 is good for this
+downsampling = 1e-3
+
+# Seed for the RNG, to make the results reproducible.
+#random number generator
+#deterministic, good for debugging
+seed = 1
+
+thrones2vec = w2v.Word2Vec(
+    sg=1,
+    seed=seed,
+    workers=num_workers,
+    size=num_features,
+    min_count=min_word_count,
+    window=context_size,
+    sample=downsampling
+)
+thrones2vec.build_vocab(sentences)
+print("Word2Vec vocabulary length:", len(thrones2vec.wv.vocab))
+thrones2vec.train(sentences,total_examples=thrones2vec.corpus_count,epochs=thrones2vec.iter)
+#if not os.path.exists("trained"):
+ #   os.makedirs("trained")
+#thrones2vec.save(os.path.join("trained", "thrones2vec.w2v"))
+thrones2vec = w2v.Word2Vec.load(os.path.join("trained", "thrones2vec.w2v"))
+#my video - how to visualize a dataset easily
+tsne = sklearn.manifold.TSNE(n_components=2, random_state=0)
+all_word_vectors_matrix = thrones2vec.wv.syn0
+
+print (thrones2vec.most_similar("Stark"))
+print (thrones2vec.most_similar("Aerys"))
+print (thrones2vec.most_similar("direwolf"))
+print (thrones2vec.most_similar("Robb"))
+def nearest_similarity_cosmul(start1, end1, end2):
+    similarities = thrones2vec.most_similar_cosmul(
+        positive=[end2, start1],
+        negative=[end1]
+    )
+    start2 = similarities[0][0]
+    print("{start1} is related to {end1}, as {start2} is related to {end2}".format(**locals()))
+    return start2
+nearest_similarity_cosmul("Stark", "Winterfell", "Riverrun")
+nearest_similarity_cosmul("Jaime", "sword", "wine")
+nearest_similarity_cosmul("Arya", "Nymeria", "dragons")
